84c9e705913c267d509c4af093380b0d379680d9
Joshua Bartlett <joshua.r.bartlett@gmail.com>
2018-11-08 13:25:45 -0600

    Create plugin

---
+++ cd-reminder.plugin.zsh
@@ -0,0 +1,19 @@
+# calls the builtin cd command, and then optionally cat's the .cd-reminder file
+# in the directory if it exists
+reminder_cd() {
+    builtin cd "$@" && { [[ ! -f .cd-reminder ]] || cat .cd-reminder 1>&2; }
+}
+
+# Either creates an empty .cd-reminder file, or if an argument is included
+# then creates and appends to that file the first argument
+new_reminder() {
+    if [[ $# -eq 0 ]]; then
+        touch .cd-reminder
+    fi
+
+    if [[ $# -gt 0 ]]; then
+        echo $1 >> .cd-reminder
+    fi
+}
+
+alias cd=reminder_cd
